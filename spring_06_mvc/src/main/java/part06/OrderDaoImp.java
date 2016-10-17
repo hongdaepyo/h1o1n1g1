f9ebce1ce8c318242239b46d7c4c94a671b9976d
Hongdaepyo <h1o1n1g1@nate.com>
2016-10-17 15:10:32 +0900

    2016-10-17

---
+++ spring_06_mvc/src/main/java/part06/OrderDaoImp.java
@@ -0,0 +1,29 @@
+package part06;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+
+public class OrderDaoImp implements OrderDao{
+	private SqlSessionTemplate sqlSession;
+	
+	public OrderDaoImp() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void setSqlSession(SqlSessionTemplate sqlSession) {
+		this.sqlSession = sqlSession;
+	}
+
+	@Override
+	public void insertMethod(OrderDTO dto) {
+		sqlSession.insert("order.ins",dto);
+		
+	}
+
+	@Override
+	public List<OrderDTO> selectMethod() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
