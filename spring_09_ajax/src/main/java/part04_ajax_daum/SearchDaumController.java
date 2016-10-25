597dd4d9f5a747b96401df953d449db8ce7c84a3
Hongdaepyo <h1o1n1g1@nate.com>
2016-10-25 15:30:16 +0900

    2016-10-25

--- spring_09_ajax/src/main/java/part04_ajax_daum/SearchDaumController.java
+++ spring_09_ajax/src/main/java/part04_ajax_daum/SearchDaumController.java
@@ -1,13 +1,57 @@
 package part04_ajax_daum;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 //945948afbe6a4f41cd89e5fd9efe678e   다음 api키
+import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class SearchDaumController {
 	@RequestMapping("/daumForm.do")
-	public String form(){
+	public String form(){ 
 		return "part04_ajax_daum/daumForm";
 	}//end form()
-
+	
+	/*@RequestMapping("/searchOpen.do")
+	public ModelAndView process(String search) throws MalformedURLException, IOException{
+		ModelAndView mav=new ModelAndView();
+		String url="https://apis.daum.net/search/book?apikey=945948afbe6a4f41cd89e5fd9efe678e&q="+URLEncoder.encode(search,"UTF-8")+"&output=xml";
+		HttpURLConnection con=(HttpURLConnection)new URL(url).openConnection();
+		
+		BufferedReader reader=new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
+		String input=null;
+		String total="";
+		while((input=reader.readLine())!=null){
+			total+=input;
+		}
+		
+		mav.addObject("total",total);
+		mav.setViewName("part04_ajax_daum/daumResult");
+		return mav;
+	}//end process()
+*/
+	
+	@RequestMapping("/searchOpen.do")
+	public @ResponseBody BookList process(String search) throws MalformedURLException, IOException, JAXBException{
+		String url="https://apis.daum.net/search/book?apikey=945948afbe6a4f41cd89e5fd9efe678e&q="+URLEncoder.encode(search,"UTF-8")+"&output=xml";
+		HttpURLConnection con=(HttpURLConnection)new URL(url).openConnection();
+		JAXBContext jaxbContext=null;
+		BookList booklist =null;
+		jaxbContext=jaxbContext.newInstance(BookList.class);
+		booklist=(BookList)jaxbContext.createUnmarshaller().unmarshal(con.getInputStream());
+		System.out.println("data:"+booklist.getBookItem().get(0).getTitle());
+		return booklist;
+	}
 }//end class
---
+++ spring_10_daumMap3/src/main/webapp/WEB-INF/view/map.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html >
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+<script type="text/javascript" src="//apis.daum.net/maps/maps3.js?apikey=945948afbe6a4f41cd89e5fd9efe678e"></script>
+
+<script>
+
+</script>
+</head>
+<body>
+	<div class="map_wrap">
+<!-- 		<div id="map" style="width:100%; height:100%; position:relative; overflow:hidden;"> -->
+		<div id="map" style="width:500px; height:400px;">
+	</div>
+	
+	<script type="text/javascript" src="js/map.js"></script>
+</body>
+</html>
